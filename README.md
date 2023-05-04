63d43383d48e205a97b7a5b98aa5fb51745901ac
Cwill290 <cwill290@gmail.com>
2023-05-04 19:43:09 -0400

    finished the code for javascript

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Cwill-Password-Generator
+
+## Description
+
+## Usage
+
+## Credits
+
+https://www.youtube.com/watch?v=duNmhKgtcsI
+
+http://www.net-comber.com/charset.html
+
+https://web.dev/
+
+https://www.w3schools.com/
--- script.js
+++ script.js
@@ -1,14 +1,54 @@
 // Assignment code here
+let passLength;
+do{
+     passLength = prompt("Choose 8 - 20 for your password length");
+}while(passLength > 20 || passLength< 8)
+console.log(passLength)
 
-//DOM elements
-let resultEl = document.getElementById
 
-let randomFunc = {
+let choiceLower = confirm("Will your password include lowercase letters?");
+console.log(choiceLower)
+let choiceUpper = confirm("Will your password include uppercase letters?");
+console.log(choiceUpper)
+let choiceNumber = confirm("Will your password include numbers?");
+console.log(choiceNumber)
+let choiceSymbol = confirm("Will your password include symbols?");
+console.log(choiceSymbol)
+//filtering out values for password
+const filterFunc = {
     lower: getRandomLower,
     upper: getRandomUpper,
     number: getRandomNumber,
     symbol: getRandomSymbol
 }
+
+function generatePassword(lower, upper, number, symbol, passLength) {
+    let generatedPassword = "";
+    const typesCount = lower + upper + number + symbol;
+    const typesArray = [{lower}, {upper}, {number}, {symbol},].filter(constraint => Object.values(constraint)[0]);
+
+    if(typesCount === 0) {
+		return '';
+    }
+
+    for(let i=0; i<passLength; i+=typesCount) {
+		typesArray.forEach(type => {
+			const funcName = Object.keys(type)[0];
+			generatedPassword += filterFunc[funcName]();
+		});
+    }
+
+    const finalPassword = generatedPassword.slice(0, passLength);
+
+    return finalPassword;
+
+}
+
+
+
+
+//DOM elements
+let resultEl = document.getElementById("password");
 
 
 
@@ -28,7 +68,7 @@ function getRandomNumber() {
 
 function getRandomSymbol() {
     const symbols = "!@$().<>"
-    return symbols[Math.floor(Math,.random() * symbols.length)];
+    return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
 
@@ -37,12 +77,25 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+
+  var password = generatePassword(choiceLower, choiceUpper, choiceNumber, choiceSymbol, passLength);
+// console.log(password);
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
+  return passwordText.value
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", () => {
+    passLength
+    choiceLower.checked;
+    choiceUpper.checked;
+    choiceNumber.checked;
+    choiceSymbol.checked;
+
+    resultEl.innerText =  writePassword()
+});
+
+
